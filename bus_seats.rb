b39df721e80c483df25a9bcb9afb82e564bb68b1
mjbeacham <beacham.matt@gmail.com>
2022-05-01 14:50:55 +1000

    bus class initalized

--- bus_seats.rb
+++ bus_seats.rb
@@ -1,26 +1,33 @@
-seats = [{1 => true}, {2 => true}, {3 => false}, {4 => true}, {5 => false}]
+require 'json'
+require 'digest'
+require 'colorize'
+
+class Bus
+  @@alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+end
 
 # this is the main menu of the app
 loop do
-    puts '1. View available seats'
-    puts '2. Reserve seats'
-    puts '3. View help'
-    puts '4. Exit'
+  puts '1. View available seats'
+  puts '2. Reserve seats'
+  puts '3. View help'
+  puts '4. Exit'
 
-    puts 'Please select (1-4): '
-    choice = gets.chomp
+  puts 'Please select (1-4): '
+  choice = gets.chomp
 
-    case choice
-    when '1'
-        puts 'Available seats here'
-    when '2'
-        puts 'Reverse seats here'
-    when '3'
-        puts 'Help details here'
-    when '4'
-        puts 'Thank you fopr using the bus seat allocation tool! Goodbye'
-        break
-    else
-        puts 'Invalid choice. Please try again using only a number.'
-    end
+  case choice
+  when '1'
+    puts 'Available seats here'
+  when '2'
+    puts 'Reverse seats here'
+  when '3'
+    puts 'Help details here'
+  when '4'
+    puts 'Thank you fopr using the bus seat allocation tool! Goodbye'
+    break
+  else
+    puts 'Invalid choice. Please try again using only a number.'
+  end
 end
