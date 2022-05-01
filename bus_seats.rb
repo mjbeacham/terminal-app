d245d758e71d1fe2a3b52b9d0d1a5959285a685f
mjbeacham <beacham.matt@gmail.com>
2022-05-01 14:56:29 +1000

    class methods for seat allocation created

--- bus_seats.rb
+++ bus_seats.rb
@@ -5,6 +5,24 @@ require 'colorize'
 class Bus
   @@alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+  def initialize(rows, seats, bookings_)
+    @@rows = rows
+    @@seats = seats
+    @@bookings = bookings
+  end
+
+  def get_rows
+    return @@rows
+  end
+
+  def book(seat)
+    if !@@bookings[seat]
+      @@bookings[seat] = Digest::SHA256.hexdigest(seat).slice(0,8).upcase!ARGF
+      puts "Congratulations, you have booked a seat " + seat.green ". Your ticket number is: " + @@bookings[seat].green
+    else
+      puts "Sorry, " + seat.red + " is already booked."
+    end
+
 end
 
 # this is the main menu of the app
